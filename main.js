941d301cf32ba89f5d0a421294db28dfe4240900
ItzStoopy <93840089+ItzStoopy@users.noreply.github.com>
2023-01-06 18:27:46 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier=ml5.imageClassifier('MobileNet', modelLoaded);
+}
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier=ml5.imageClassifier('Google', modelLoaded);
+}
+
+function modelLoaded() {
+console.log('modelLoaded');
+}
+function draw () {
+image(video,0,0,300,300);
+classifier.classify(video,gotResult)
+}
+var previous_result='';
+function gotResult(error,results){
+if(error){
+console.error(error);
+}
+else {
+if ((results[0].confidence>0.5)&&(previous_result !=results[0].label)){
+console.log(results)
+
+}
+}
+}
